37f7c1dcfa873aca343d9a9e135fdac6196b2645
BlaydeRunner08 <erikbeerda@gmail.com>
2024-04-25 13:40:09 -0700

    Add files via upload

---
+++ CBA.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"syscall"
+	"time"
+)
+
+const port = 80
+
+var keyboardFd int
+var err error
+var msgChan chan string
+
+func sendKey(key byte) {
+	modifier := byte(0x00)
+	hidCode := byte(0x00)
+
+	// 'a'-'z'
+	if key >= 0x61 && key <= 0x7a {
+		hidCode = key - 0x5d
+	}
+
+	// 'A'-'Z'
+	if key >= 0x41 && key <= 0x5a {
+		hidCode = key - 0x3d
+		modifier = 0x20
+	}
+
+	// space
+	if key == 0x20 {
+		hidCode = 0x2c
+	}
+
+	// minus (used as a divider)
+	if key == 0x2d {
+		hidCode = 0x2d
+	}
+		
+	if hidCode != 0 {
+		syscall.Write(keyboardFd, []byte{modifier, 0, hidCode, 0, 0, 0, 0, 0})
+		releaseKeys()
+	}
+}
+
+func releaseKeys() {
+	time.Sleep(10 * time.Millisecond)
+	syscall.Write(keyboardFd, []byte{0, 0, 0, 0, 0, 0, 0, 0})
+	time.Sleep(10 * time.Millisecond)
+}
+
+func pressKeyCode(keyCode byte) {
+	syscall.Write(keyboardFd, []byte{0, 0, 0, keyCode, 0, 0, 0, 0})
+	releaseKeys()
+}
+
+func pressEnter() {
+	pressKeyCode(0x28)
+}
+
+func pressEsc() {
+	pressKeyCode(0x29)
+}
+
+func getTimestamp() int64 {
+	return time.Now().UnixNano() / int64(time.Millisecond)
+}
+
+func sendHandler(w http.ResponseWriter, r *http.Request) {
+	msg, ok := r.URL.Query()["msg"]
+
+	if !ok || len(msg[0]) < 1 {
+		log.Println("request param 'msg' is missing")
+		return
+	}
+
+	log.Println("Sending keyboard msg: " + string(msg[0]))
+
+	msgChan <- string(msg[0])
+}
+
+func processMessages() {
+	for {
+		msg := <-msgChan
+		byteData := []byte(msg)
+
+		pressEnter()
+
+		for _, key := range byteData {
+			sendKey(key)
+		}
+		
+		pressEnter()
+		pressEsc()
+	}
+}
+
+func main() {
+	msgChan = make(chan string, 32)
+
+	keyboardFd, err = syscall.Open("/dev/hidg0", syscall.O_RDWR, 06666)
+
+	if err != nil {
+		fmt.Print(err.Error(), "\n")
+		return
+	}
+
+	go processMessages()
+
+	fileServer := http.FileServer(http.Dir("/usr/local/bin/CheckpointBot_Assistant/checkpointbot"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/send", sendHandler)
+
+	fmt.Printf("callouts server starting on port %d\n", port)
+
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
+		log.Fatal(err)
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# CheckpointBot_Assistant
-Assistant for Xbox players to access CheckpointBot using Raspberry Pi
+# Checkpoint Bot Assistant
+
+This is a program to help Xbox users access the https://beta.d2checkpoint.com/ checkpoint bots without having to use a keyboard.
+
+Runs on Raspberry Pi devices that support OTG-USB on /dev/hidg0.  
+
+Connect Raspberry Pi's USB-OTG port to console USB port to act as a USB keyboard.
+
+clone this repo to /usr/local/bin, and set up a cronjob to "@reboot sudo go run /usr/local/bin/CheckpointBot_Assistant/CBA.go&"
+
+Run callouts server on Raspberry Pi, connect to server from any browser 
+(PC/mobile/tablet) to get web UI to select icons to transmit callouts over text chat without having to type them in.
+
+
+
+Developed and tested using a Raspberry Pi Zero W and Xbox Series X using a standard USB cable.
+
+
+
